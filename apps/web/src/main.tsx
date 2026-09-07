4089cf5763127c8c8e8a15b6cb38693e2454857f
qasimkdr <kdrqasim@gmail.com>
2026-09-07 14:02:09 +0500

    feat: mount subscription entitlement provider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
 import { AuthProvider } from './features/auth/AuthProvider';
+import { SubscriptionProvider } from './features/subscription/SubscriptionProvider';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <SubscriptionProvider>
+          <App />
+        </SubscriptionProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
